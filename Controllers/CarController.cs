804aecf67bdeb38d284653bbdc5ae73515df8e6a
menna sobeh <mennasopeh@gmail.com>
2023-03-14 01:29:02 +0200

    Add project files.

---
+++ Controllers/CarController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLab.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars = Models.Car.GetCars();
+            return View(cars);
+        }
+    }
+}
+
